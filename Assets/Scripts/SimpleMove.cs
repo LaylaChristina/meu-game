535bd02dfaedc695f9b40ad024b14aeb035e09d8
LaylaChristina <laylachristinaferreiranaves@gmail.com>
2019-03-11 09:33:45 -0300

    Primeiro Script: Update de posição

--- Assets/Scripts/SimpleMove.cs
+++ Assets/Scripts/SimpleMove.cs
@@ -13,6 +13,6 @@ public class SimpleMove : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        transform.position = new Vector3(transform.position.x + 0.1f, transform.position.y, transform.position.z);
     }
 }
