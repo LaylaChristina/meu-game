6462df4c1af133270580683962eb1bc9df07a494
Layla Christina <laylachristinaferreiranaves@gmail.com>
2019-05-13 10:08:41 -0300

    CameraFollow

--- Assets/Scripts/PlayerControl.cs
+++ Assets/Scripts/PlayerControl.cs
@@ -21,7 +21,7 @@ public class PlayerControl : MonoBehaviour
         if (other.gameObject.CompareTag("Gift")){
             StopMusicAndTape();
             AudioManager.instance.PlaySoundLevelComplete(gameObject);
-            Destroy(gameObject);
+            DestroyPlayer();
             ////
             LevelManager.instance.ShowLevelCompletePanel();
 
@@ -44,13 +44,19 @@ public class PlayerControl : MonoBehaviour
             StopMusicAndTape();
             AudioManager.instance.PlaySoundFail(gameObject);
             SFXManager.instance.ShowDieParticles(gameObject);
-            Destroy(gameObject);
+            DestroyPlayer();
             LevelManager.instance.ShowGameOverPanel();
         }
 
         void Impulse(float force){
             rb.velocity = Vector3.zero;
             rb.AddForce(Vector3.up*force, ForceMode2D.Impulse);
+        }
+
+        void DestroyPlayer(){
+            Camera.main.GetComponent<CameraFollow>().TurnOff();
+            Destroy(gameObject);
+
         }
 
 
