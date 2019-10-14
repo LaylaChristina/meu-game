3f6cd334c5fb065e3a5e072e67c0682312d3e068
LaylaChristina <laylachristinaferreiranaves@gmail.com>
2019-10-14 10:54:11 -0300

    Adicionando introdução

---
+++ Assets/Scripts/TypeWriter.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class TypeWriter : MonoBehaviour
+{
+    public float delay = 0.1f;
+    public string fullText;
+    public string fullText2;
+    public string fullText3;
+    private string currentText = "";
+
+    void Start()
+    {
+        StartCoroutine(ShowText());
+    }
+
+    IEnumerator ShowText (){
+        for (int i =0; i<fullText.Length;i++ ){
+            currentText = fullText.Substring(0, i);
+            this.GetComponent<Text>().text = currentText;
+            yield return new WaitForSeconds(delay);
+        }
+        yield return new WaitForSeconds(3f);
+        this.GetComponent<Text>().text="";
+
+         for (int i =0; i<fullText2.Length;i++ ){
+            currentText = fullText2.Substring(0, i);
+            this.GetComponent<Text>().text = currentText;
+            yield return new WaitForSeconds(delay);
+        }
+        yield return new WaitForSeconds(3f);
+        this.GetComponent<Text>().text="";
+
+        for (int i =0; i<fullText3.Length;i++ ){
+            currentText = fullText3.Substring(0, i);
+            this.GetComponent<Text>().text = currentText;
+            yield return new WaitForSeconds(delay);
+        }
+        yield return new WaitForSeconds(3f);
+        this.GetComponent<Text>().text="";
+    }
+
+}
